18e9bbc08e2d3c26fbce8d17d5d1c15694eeacf4
Harsh Jain <jainh9999@gmail.com>
2018-11-10 14:11:29 -0500

    Home Page (not fully working)

--- BrainyStories/BrainyStories/BrainyStories/App.xaml.cs
+++ BrainyStories/BrainyStories/BrainyStories/App.xaml.cs
@@ -3,29 +3,23 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-namespace BrainyStories
-{
-    public partial class App : Application
-    {
-        public App()
-        {
+namespace BrainyStories {
+    public partial class App : Application {
+        public App() {
             InitializeComponent();
 
             MainPage = new MainPage();
         }
 
-        protected override void OnStart()
-        {
+        protected override void OnStart() {
             // Handle when your app starts
         }
 
-        protected override void OnSleep()
-        {
+        protected override void OnSleep() {
             // Handle when your app sleeps
         }
 
-        protected override void OnResume()
-        {
+        protected override void OnResume() {
             // Handle when your app resumes
         }
     }
--- BrainyStories/BrainyStories/BrainyStories/MainPage.xaml.cs
+++ BrainyStories/BrainyStories/BrainyStories/MainPage.xaml.cs
@@ -5,12 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace BrainyStories
-{
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
+namespace BrainyStories {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
             InitializeComponent();
         }
     }
