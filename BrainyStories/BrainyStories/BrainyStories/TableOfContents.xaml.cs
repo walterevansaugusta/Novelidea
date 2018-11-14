d77bd782712fa1d8d335e2f471cc5ed1f7e81827
Brooke White <brookewhite19981@gmail.com>
2018-11-13 21:13:14 -0500

    Working on Table of Contents

--- BrainyStories/BrainyStories/BrainyStories/TableOfContents.xaml.cs
+++ BrainyStories/BrainyStories/BrainyStories/TableOfContents.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,8 +24,34 @@ namespace BrainyStories
             var viewModel = BindingContext as TableOfContentsModel;
 
             var story = e.Item as Story;
-
+            var grid = this.FindByName("ButtonGrid");
+            Console.WriteLine(grid);
             viewModel.HideShowRead(story);
+        }
+    }
+
+    public class StatusToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Equals(value, null))
+                return new GridLength(0);
+
+            var story = value as Story;
+
+            if (story.CanRead)
+            {
+                return new GridLength(0);
+            }
+            else
+            {
+                return new GridLength(1, GridUnitType.Auto);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Only one way bindings are supported with this converter");
         }
     }
 }
