38c4b883bfeffd7ed9e4d66510212b23c0652960
Brooke White <brookewhite19981@gmail.com>
2019-01-21 18:21:11 -0500

    Added Index for Appeal

--- BrainyStories/BrainyStories/BrainyStories/TableOfContents.xaml.cs
+++ BrainyStories/BrainyStories/BrainyStories/TableOfContents.xaml.cs
@@ -18,18 +18,17 @@ namespace BrainyStories
         public TableOfContents ()
 		{
 			InitializeComponent();
+            CreateAppealKey();
             NavigationPage.SetHasNavigationBar(this, false);
 
             // TO-DO: 
-            // make these into their own class for better code
-            // add icons below title for quizes and home expierences
-            /// add clickability
+            // remove whitespace btw cols
 
             StoryFactory storyFact = new StoryFactory();
             storyList = storyFact.generateStories();
 
-            gridLayout.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50) });
-
+            //gridLayout.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50) });
+            gridLayout.RowDefinitions.Add(new RowDefinition());
             var storyIndex = 0;
             var rowNum = 0;
             for (int storyNum = 0; storyNum < storyList.Count; storyNum+=3 )
@@ -39,22 +38,102 @@ namespace BrainyStories
                     var story = storyList[storyIndex];
                     storyIndex += 1;
                     var image = new Image { Source = story.Icon };
+                    var quiz = new Image { Source = "QuizzesIcon.png" };
+                    var think = new Image { Source = "ThinkAndDoIcon.png" };
                     var label = new Label
                     {
                         Text = story.Name,
                         //VerticalOptions = LayoutOptions.Center,
                         HorizontalOptions = LayoutOptions.Center
                     };
-
-                    gridLayout.Children.Add(image, columnIndex, rowNum);
-                    Grid.SetRowSpan(image, 2);
-                    gridLayout.Children.Add(label, columnIndex, rowNum + 1);
-
-                    gridLayout.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50) });
-                    gridLayout.Children.Add(label, columnIndex, rowNum + 2);
+                    var horizontalStack = new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        HorizontalOptions = LayoutOptions.FillAndExpand,
+                        Children = {
+                            quiz,
+                            think
+                        }
+                    };
+                    var verticalStack = new StackLayout
+                    {
+                        Orientation = StackOrientation.Vertical,
+                        HorizontalOptions = LayoutOptions.FillAndExpand,
+                        Children =
+                        {
+                            image,
+                            label,
+                            horizontalStack
+                        }
+                    };
+                    
+                    gridLayout.Children.Add(verticalStack, columnIndex, rowNum);
+                    //gridLayout.RowDefinitions.Add(new RowDefinition());
                 }
-                rowNum += 3;
+                rowNum += 2;
             }
+        }
+
+        void CreateAppealKey()
+        {
+            var general = new Label()
+            {
+                Text = "General",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            var male = new Label()
+            {
+                Text = "Male",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            var female = new Label()
+            {
+                Text = "Female",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            var animal = new Label()
+            {
+                Text = "Animals",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            var generalDot = new Image()
+            {
+                Source = "GeneralAppeal.png",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            var maleDot = new Image()
+            {
+                Source = "MaleProtagonist.png",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            var femaleDot = new Image()
+            {
+                Source = "FemaleProtagonist.png",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            var animalDot = new Image()
+            {
+                Source = "AnimalAppeal.png",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            horizontalStackLayout.Orientation = StackOrientation.Horizontal;
+            horizontalStackLayout.HorizontalOptions = LayoutOptions.FillAndExpand;
+            horizontalStackLayout.Children.Add(generalDot);
+            horizontalStackLayout.Children.Add(general);
+            horizontalStackLayout.Children.Add(animalDot);
+            horizontalStackLayout.Children.Add(animal);
+            horizontalStackLayout.Children.Add(femaleDot);
+            horizontalStackLayout.Children.Add(female);
+            horizontalStackLayout.Children.Add(maleDot);
+            horizontalStackLayout.Children.Add(male);
         }
     }
 }
