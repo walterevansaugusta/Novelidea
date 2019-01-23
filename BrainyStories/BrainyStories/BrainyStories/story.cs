b39d789387bc407b8d834f4688f1961f79565781
Lindsey <lstowell3@gatech.edu>
2019-01-22 21:08:37 -0500

    Updated Story class and StoryFactory to include appeal, number of quizzes, and number of think and dos

--- BrainyStories/BrainyStories/BrainyStories/story.cs
+++ BrainyStories/BrainyStories/BrainyStories/story.cs
@@ -5,11 +5,34 @@ using Xamarin.Forms;
 
 namespace BrainyStories
 {
+    public class AppealType
+    {
+        private AppealType(string value) { Value = value; }
+
+        public string Value { get; set; }
+
+        public static AppealType Male { get { return new AppealType("Male.png"); } }
+        public static AppealType Female { get { return new AppealType("Female.png"); } }
+        public static AppealType General { get { return new AppealType("General.png"); } }
+        public static AppealType Animal { get { return new AppealType("Animal.png"); } }
+    }
+
     public class Story
     {
-        public String Name { get; set; }
+        // Name of story/imagine
+        public String Name { get; set; } 
 
+        // Image clip for icon
         public ImageSource Icon { get; set; }
+
+        // Appeal type for colored dots
+        public AppealType Appeal { get; set; }
+
+        // Number of quizzes for story/imagine
+        public int QuizNum { get; set; }
+
+        // Number of think and do exercises
+        public int ThinkDoNum { get; set; }
 
     }
 }
