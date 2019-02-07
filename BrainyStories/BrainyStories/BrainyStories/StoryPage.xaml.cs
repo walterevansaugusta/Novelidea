122aa2142795f538402c7fb9a39acae6244cc62c
Brooke White <brookewhite19981@gmail.com>
2019-02-07 10:47:28 -0500

    Added test slider

--- BrainyStories/BrainyStories/BrainyStories/StoryPage.xaml.cs
+++ BrainyStories/BrainyStories/BrainyStories/StoryPage.xaml.cs
@@ -16,7 +16,44 @@ namespace BrainyStories
 		{
 			InitializeComponent();
             testLabel.Text = story.Name;
+            Label rotationLabel = new Label
+            {
+                Text = "TEST",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
 
-		}
+            Label displayLabel = new Label
+            {
+                Text = "0",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Slider slider = new Slider
+            {
+                Maximum = story.Duration.Seconds + (story.Duration.Minutes * 60),
+                Minimum = 0,
+                Value = 0
+            };
+            slider.ValueChanged += (sender, args) =>
+            {
+                rotationLabel.Rotation = slider.Value;
+                displayLabel.Text = String.Format("The Slider value is {0}", args.NewValue);
+            };
+
+            Title = "Basic Slider Code";
+            Padding = new Thickness(10, 10);
+            Content = new StackLayout
+            {
+                Children =
+            {
+                rotationLabel,
+                displayLabel,
+                slider
+            }
+            };
+        }
 	}
 }
