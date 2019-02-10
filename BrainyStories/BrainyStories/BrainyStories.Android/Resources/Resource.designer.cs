a1b7646d3d431e1df363a3589774c8bfddbaf22f
ctrively <claretrively@gatech.edu>
2019-02-10 18:49:48 -0500

    the gingerbread man slides

--- BrainyStories/BrainyStories/BrainyStories.Android/Resources/Resource.designer.cs
+++ BrainyStories/BrainyStories/BrainyStories.Android/Resources/Resource.designer.cs
@@ -2345,26 +2345,26 @@ namespace BrainyStories.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020176
-			public const int avd_hide_password_1 = 2130837878;
+			// aapt resource value: 0x7f020181
+			public const int avd_hide_password_1 = 2130837889;
 			
-			// aapt resource value: 0x7f020177
-			public const int avd_hide_password_2 = 2130837879;
+			// aapt resource value: 0x7f020182
+			public const int avd_hide_password_2 = 2130837890;
 			
-			// aapt resource value: 0x7f020178
-			public const int avd_hide_password_3 = 2130837880;
+			// aapt resource value: 0x7f020183
+			public const int avd_hide_password_3 = 2130837891;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f020179
-			public const int avd_show_password_1 = 2130837881;
+			// aapt resource value: 0x7f020184
+			public const int avd_show_password_1 = 2130837892;
 			
-			// aapt resource value: 0x7f02017a
-			public const int avd_show_password_2 = 2130837882;
+			// aapt resource value: 0x7f020185
+			public const int avd_show_password_2 = 2130837893;
 			
-			// aapt resource value: 0x7f02017b
-			public const int avd_show_password_3 = 2130837883;
+			// aapt resource value: 0x7f020186
+			public const int avd_show_password_3 = 2130837894;
 			
 			// aapt resource value: 0x7f020056
 			public const int ClassicStories = 2130837590;
@@ -3020,11 +3020,11 @@ namespace BrainyStories.Droid
 			// aapt resource value: 0x7f02012f
 			public const int notification_icon_background = 2130837807;
 			
-			// aapt resource value: 0x7f020174
-			public const int notification_template_icon_bg = 2130837876;
+			// aapt resource value: 0x7f02017f
+			public const int notification_template_icon_bg = 2130837887;
 			
-			// aapt resource value: 0x7f020175
-			public const int notification_template_icon_low_bg = 2130837877;
+			// aapt resource value: 0x7f020180
+			public const int notification_template_icon_low_bg = 2130837888;
 			
 			// aapt resource value: 0x7f020130
 			public const int notification_tile_bg = 2130837808;
@@ -3162,73 +3162,106 @@ namespace BrainyStories.Droid
 			public const int S7_PR_9 = 2130837852;
 			
 			// aapt resource value: 0x7f02015d
-			public const int slide1 = 2130837853;
+			public const int S9_R_1 = 2130837853;
 			
 			// aapt resource value: 0x7f02015e
-			public const int slide10 = 2130837854;
+			public const int S9_R_10 = 2130837854;
 			
 			// aapt resource value: 0x7f02015f
-			public const int slide11 = 2130837855;
+			public const int S9_R_11 = 2130837855;
 			
 			// aapt resource value: 0x7f020160
-			public const int slide12 = 2130837856;
+			public const int S9_R_12 = 2130837856;
 			
 			// aapt resource value: 0x7f020161
-			public const int slide13 = 2130837857;
+			public const int S9_R_13 = 2130837857;
 			
 			// aapt resource value: 0x7f020162
-			public const int slide14 = 2130837858;
+			public const int S9_R_14 = 2130837858;
 			
 			// aapt resource value: 0x7f020163
-			public const int slide15 = 2130837859;
+			public const int S9_R_15 = 2130837859;
 			
 			// aapt resource value: 0x7f020164
-			public const int slide16 = 2130837860;
+			public const int S9_R_16 = 2130837860;
 			
 			// aapt resource value: 0x7f020165
-			public const int slide17 = 2130837861;
+			public const int S9_R_17 = 2130837861;
 			
 			// aapt resource value: 0x7f020166
-			public const int slide18 = 2130837862;
+			public const int S9_R_18 = 2130837862;
 			
 			// aapt resource value: 0x7f020167
-			public const int slide19 = 2130837863;
+			public const int S9_R_19 = 2130837863;
 			
 			// aapt resource value: 0x7f020168
-			public const int slide2 = 2130837864;
+			public const int S9_R_2 = 2130837864;
 			
 			// aapt resource value: 0x7f020169
-			public const int slide3 = 2130837865;
+			public const int S9_R_3 = 2130837865;
 			
 			// aapt resource value: 0x7f02016a
-			public const int slide4 = 2130837866;
+			public const int S9_R_4 = 2130837866;
 			
 			// aapt resource value: 0x7f02016b
-			public const int slide5 = 2130837867;
+			public const int S9_R_5 = 2130837867;
 			
 			// aapt resource value: 0x7f02016c
-			public const int slide6 = 2130837868;
+			public const int S9_R_6 = 2130837868;
 			
 			// aapt resource value: 0x7f02016d
-			public const int slide7 = 2130837869;
+			public const int S9_R_7 = 2130837869;
 			
 			// aapt resource value: 0x7f02016e
-			public const int slide8 = 2130837870;
+			public const int S9_R_8 = 2130837870;
 			
 			// aapt resource value: 0x7f02016f
-			public const int slide9 = 2130837871;
+			public const int S9_R_9 = 2130837871;
 			
 			// aapt resource value: 0x7f020170
-			public const int ThinkAndDo = 2130837872;
+			public const int slide1 = 2130837872;
 			
 			// aapt resource value: 0x7f020171
-			public const int ThinkAndDoIcon = 2130837873;
+			public const int slide10 = 2130837873;
 			
 			// aapt resource value: 0x7f020172
-			public const int tooltip_frame_dark = 2130837874;
+			public const int slide11 = 2130837874;
 			
 			// aapt resource value: 0x7f020173
-			public const int tooltip_frame_light = 2130837875;
+			public const int slide2 = 2130837875;
+			
+			// aapt resource value: 0x7f020174
+			public const int slide3 = 2130837876;
+			
+			// aapt resource value: 0x7f020175
+			public const int slide4 = 2130837877;
+			
+			// aapt resource value: 0x7f020176
+			public const int slide5 = 2130837878;
+			
+			// aapt resource value: 0x7f020177
+			public const int slide6 = 2130837879;
+			
+			// aapt resource value: 0x7f020178
+			public const int slide7 = 2130837880;
+			
+			// aapt resource value: 0x7f020179
+			public const int slide8 = 2130837881;
+			
+			// aapt resource value: 0x7f02017a
+			public const int slide9 = 2130837882;
+			
+			// aapt resource value: 0x7f02017b
+			public const int ThinkAndDo = 2130837883;
+			
+			// aapt resource value: 0x7f02017c
+			public const int ThinkAndDoIcon = 2130837884;
+			
+			// aapt resource value: 0x7f02017d
+			public const int tooltip_frame_dark = 2130837885;
+			
+			// aapt resource value: 0x7f02017e
+			public const int tooltip_frame_light = 2130837886;
 			
 			static Drawable()
 			{
