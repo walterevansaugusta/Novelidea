51edb49ed2ceb3ce8b511167bdf8705b6d4d3eaa
Brooke White <brookewhite19981@gmail.com>
2019-03-11 17:07:04 -0400

    Made quiz object

--- BrainyStories/BrainyStories/BrainyStories/Objects/Quiz.cs
+++ BrainyStories/BrainyStories/BrainyStories/Objects/Quiz.cs
@@ -3,7 +3,24 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace BrainyStories.Objects {
+    public class Question
+    {
+        // The text of the question
+        public string QuestionText { get; set; }
+
+        // the array of answers
+        public string[] AnswerArray { get; set; }
+
+        //the index of the correct answer in the array
+        public int CorrectAnswer { get; set; }
+
+    }
     public class Quiz {
 
+        // the score for the quiz
+        public int Score { get; set; }
+
+        // the list of questions
+        public Question[] Questions { get; set; }
     }
 }
